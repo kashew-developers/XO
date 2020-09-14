abcf46646476f17c39ce79c309403cf4391d76c4
kashew-developers <kashewdevelopers@gmail.com>
2020-09-14 16:56:20 +0530

    Update MainActivity, change UI

--- app/src/main/java/in/kashewdevelopers/xo/MainActivity.java
+++ app/src/main/java/in/kashewdevelopers/xo/MainActivity.java
@@ -1,77 +1,40 @@
 package in.kashewdevelopers.xo;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
 
     private boolean backPressed = false;
     private Toast pressBackAgain;
 
-
+    @SuppressLint("ShowToast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pressBackAgain = Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT);
+        pressBackAgain = Toast.makeText(this, R.string.press_back, Toast.LENGTH_SHORT);
 
         Typeface typeface = Typeface.createFromAsset(this.getAssets(), "fonts/paintfont.ttf");
         ((TextView) findViewById(R.id.x)).setTypeface(typeface);
         ((TextView) findViewById(R.id.and)).setTypeface(typeface);
         ((TextView) findViewById(R.id.o)).setTypeface(typeface);
-
-    }
-
-
-    public void onSinglePlayerClicked(View view) {
-
-        pressBackAgain.cancel();
-        Intent i = new Intent(this, GamePlayActivity.class);
-        i.putExtra("singlePlayer", true);
-        startActivity(i);
-
-    }
-
-
-    public void onMultiPlayerClicked(View view) {
-
-        pressBackAgain.cancel();
-        Intent i = new Intent(this, GamePlayActivity.class);
-        i.putExtra("singlePlayer", false);
-        startActivity(i);
-
-    }
-
-
-    public void onPlayWithFriendsClicked(View view){
-
-        pressBackAgain.cancel();
-        Intent i = new Intent(this, AuthenticateUser.class);
-        startActivity(i);
-
-    }
-
-
-    public void onAboutClicked(View view){
-
-        startActivity(new Intent(this, AboutActivity.class));
-
     }
 
     @Override
     public void onBackPressed() {
-
         if (backPressed) {
             pressBackAgain.cancel();
             super.onBackPressed();
+            return;
         }
 
         backPressed = true;
@@ -83,4 +46,27 @@ public class MainActivity extends Activity {
             }
         }, 2000);
     }
+
+
+    // handle widget clicks
+    public void playWithAIClicked(View view) {
+        Intent i = new Intent(this, GamePlayActivity.class);
+        i.putExtra(getString(R.string.playWithAI), true);
+        startActivity(i);
+    }
+
+    public void twoPlayerClicked(View view) {
+        Intent i = new Intent(this, GamePlayActivity.class);
+        i.putExtra(getString(R.string.twoPlayer), false);
+        startActivity(i);
+    }
+
+    public void playOnlineClicked(View view) {
+        startActivity(new Intent(this, AuthenticateUser.class));
+    }
+
+    public void aboutClicked(View view) {
+        startActivity(new Intent(this, AboutActivity.class));
+    }
+
 }
